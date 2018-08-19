68df243e0f26796a3fc06b2b34897d6281745fb6
Christian Moser <mail@christian-moser.ch>
2018-08-19 21:47:02 +0200

    - allow to stream one "block" via flac event streaming

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,7 +1,7 @@
 ART = 'art-default.jpg'
 ICON = 'icon-default.jpg'
-NAME = 'Radio Riff - Temple of Rock'
-STREAM_URL = 'http://radioriff.frequency1.com:8888/stream'
+NAME = 'Radio Paradise'
+STREAM_META = 'https://api.radioparadise.com/api/get_block?bitrate=4&info=true'
 
 ####################################################################################################
 def Start():
@@ -11,29 +11,29 @@ def Start():
 	TrackObject.thumb = R(ICON)
 
 ####################################################################################################     
-@handler('/music/radioriff', NAME, thumb=ICON, art=ART)
+@handler('/music/radioparadise', NAME, thumb=ICON, art=ART)
 def MainMenu():
 
+	meta = JSON.ObjectFromURL(url = STREAM_META)
+	Log.Debug(meta['url'])
 	oc = ObjectContainer()
-	oc.add(CreateTrackObject(url=STREAM_URL, title=NAME))
+	oc.add(CreateTrackObject(url=meta['url'] + '?src=alexa', parts = meta['song'], title=NAME))
 
 	return oc
 
 ####################################################################################################
-def CreateTrackObject(url, title, include_container=False, **kwargs):
+def CreateTrackObject(url, parts, title, include_container=False, **kwargs):
 
 	track_object = TrackObject(
-		key = Callback(CreateTrackObject, url=url, title=title, include_container=True),
+		key = Callback(CreateTrackObject, url=url, parts = parts, title=title, include_container=True),
 		rating_key = url,
 		title = title,
 		items = [
 			MediaObject(
-				parts = [
-					PartObject(key=Callback(PlayAudio, url=url, ext='mp3'))
-				],
-				container = Container.MP3,
-				bitrate = 128,
-				audio_codec = AudioCodec.MP3,
+				parts = getParts(parts),
+				container = Container.FLAC,
+				bitrate = 900,
+				audio_codec = AudioCodec.FLAC,
 				audio_channels = 2
 			)
 		]
@@ -43,6 +43,18 @@ def CreateTrackObject(url, title, include_container=False, **kwargs):
 		return ObjectContainer(objects=[track_object])
 	else:
 		return track_object
+
+
+def getParts(songs):
+	def toPartObject(songJson):
+  		songsPart = PartObject(key='https://apps.radioparadise.com/blocks/chan/0/4/' + songJson['event'] + '.flac', duration = songJson['duration'])
+		Log.Debug(songsPart.key)
+		Log.Debug(songsPart.duration)
+		return songsPart
+
+	parts = map(toPartObject, songs.values())
+	Log.Debug(parts)
+	return parts[0]
 
 ####################################################################################################
 def PlayAudio(url):
